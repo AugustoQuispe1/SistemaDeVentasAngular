6cbd5cdaa194e87d5cdc1ef4613b0ff7a16ab29d
augus <augus@EVA>
2024-07-30 14:08:27 -0300

    Agregar archivos de proyecto.

---
+++ SistemaVenta.IOC/Dependecia.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.Datos.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependecia
+    {
+        //agregar dependecias con un servicio de ASP.NET (metodo de extension)
+        public static void InyectarDependecias(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+
+    }
+}
